d3ad98a5d0d8ef30e1352c02fdd1513bb82e94dc
BINDHU1306 <bindhuchitturi6@gmail.com>
2025-02-06 12:25:58 +0530

    Update Demo067.java

--- Demo067.java
+++ Demo067.java
@@ -2,11 +2,11 @@ class Demo067{
     public static void main(String[] args) {
         int[] arr = {9,8,7,6,5,4,3,2,1};
         int n = arr.length;
-        for(int i : arr) {
+        for(int i = 0; i < n; i++){
             if(i%2==0){
-                System.out.println(i);
-            }
-            i++;
+                System.out.println("the even index numbers are:"+i)
+                    }
         }
     }
 }
+        
